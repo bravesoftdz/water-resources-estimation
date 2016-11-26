124fb9f9570d28071e64ac3d0de49d7f97e0a464
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-11-26 10:43:03 -0200

    Correção na leitura de Ia.

--- parse.py
+++ parse.py
@@ -1,5 +1,7 @@
 import sys
 
+dt = list(range(0, 3000, 30)) #0 a 3000, step = 30
+
 class SubBacia:
 	"""docstring for SubBacia"""
 	#def __init__(self, area, cn, k, n):
@@ -13,6 +15,7 @@ class SubBacia:
 		self.cn = 0.0
 		self.k = 0.0
 		self.n = 0.0
+		self.ia = 0.0
 		self.leituras = []
 
 
@@ -24,6 +27,7 @@ with open('in.txt','r') as f:
 	subBacias = [SubBacia() for i in range(int(nBacias))]
 	content = f.read().splitlines()
 	i = 0
+	leuIa = False
 	for line in content:
 		valores = line.split()
 		if(len(valores) >= 2):
@@ -32,12 +36,22 @@ with open('in.txt','r') as f:
 			subBacias[i].k = float(valores[2])
 			subBacias[i].n = float(valores[3])
 			i+=1
+			leuIa = False
 		else:
-			subBacias[i-1].leituras.append(float(line))
+			print('\t' + str(leuIa))
+			if not leuIa:
+				print("lendo Ia")
+				print(subBacias[i-1].ia)
+				subBacias[i-1].ia = float(line)
+				print(subBacias[i-1].ia)
+				leuIa = True
+			else:
+				subBacias[i-1].leituras.append(float(line))
+				
 		#print(i)
-	i = 1
-	for sb in subBacias:
-		print("SubBacia "  + str(i) + "   area: " + str(sb.area) + "   cn: " + str(sb.cn) + "   k: " + str(sb.k) + "   n: " + str(sb.n))
-		for l in sb.leituras:
-			print(l)
-		i+=1
+i = 1
+for sb in subBacias:
+	print("SubBacia "  + str(i) + "   area: " + str(sb.area) + "   cn: " + str(sb.cn) + "   k: " + str(sb.k) + "   n: " + str(sb.n) + "   Ia: " + str(sb.ia))
+	for l in sb.leituras:
+		print(l)
+	i+=1
