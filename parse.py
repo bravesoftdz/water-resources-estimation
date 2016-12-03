d2d899350946dd6167b8e1d7703029a37676c3ee
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-12-03 16:01:03 -0200

    Todos cálculos das tabelas S1 a S7 do excel recebido funcionam. Foram adicionados comentários no código.

--- parse.py
+++ parse.py
@@ -1,9 +1,10 @@
 import sys
 import math 
 
-dt = list(range(0, 3000, 30)) #0 a 3000, step = 30
+dt = list(range(0, 3000, 30)) #0 a 3000, step = 30, representa a coluna A
 
 def initialize(datafile='in.txt'):
+	""" Função para ler o arquivo de entrada, parseá-lo e retornar as n sub-bacias lidas dele. """
 	with open(datafile,'r') as f:
 		nBacias = f.readline()
 		print("nbacias: ",nBacias)
@@ -30,20 +31,30 @@ def initialize(datafile='in.txt'):
 	return subBacias
 
 class SubBacia:
-	"""docstring for SubBacia"""
+	"""docstring for SubBacia
+		Abaixo está o significado de cada váriavel utilizada pela classe e em seguida os métodos utilizados para
+		os cálculos feitos na tabela excel recebida.
+	"""
 	def __init__(self):
-		self.area = 0.0
-		self.cn = 0.0
-		self.k = 0.0
-		self.s_mm = 0.0 # coluna N
-		self.n = 0.0
-		self.ia = 0.0
-		self.leituras = []	#aka precipitação mm
-		self.hui = list(dt)
-		self.verificacaoPu = 0.0
-		self.pacum = []
-		self.pefacum = []
-		self.pefIntervalo = []
+		self.area = 0.0	# valor lido da entrada, area da sub-bacia, representa a coluna D
+		self.cn = 0.0	# valor lido da entrada, parametro sendo estimado, representa a coluna M
+		self.k = 0.0	# valor lido da entrada, parametro sendo estimado, representa a coluna B
+		self.n = 0.0	# valor lido da entrada, parametro sendo estimado, representa a coluna C
+		self.ia = 0.0	# valor lido da entrada, representa coluna K
+		self.leituras = []	# valores lidos da entrada, leituras de precipitação em mm. representa a coluna J
+		self.hui = list(dt) # lista com os valores calculados de HUI, representa as colunas E a H
+		self.verificacaoPu = 0.0 # cálculo da "Verificação de Pu", representa a coluna I
+		self.pacum = [] # lista que contém os valores de "Pacum", representa a coluna L
+		self.s_mm = 0.0 # cálculo do valor s, representa a coluna N
+		self.pefacum = [] # lista que contém os valores de "Pefacum", representa a coluna O
+		self.pefIntervalo = [] # lista que contém os valores de "Pef intervalo", representa a coluna P
+		self.qSimulado = [] # lista que contém os valores de "Q Simulado", representa a coluna BQ
+							# Todas colunas de Q a BP, que são usadas para a soma armazenada em BQ
+							# foram abstraídas com esta lista. Pois a mesma é inicializada com 0 em todas posições
+							# e cada valor calculado é somado com o valor que já estava na posição para a qual ele foi designado
+		self.verificaçãoPe = 0.0 # cálculo da "Verificação de Pe", representa a coluna BR
+		self.qp = 0.0		# variável que contém o maior valor verificado em qSimulado, calculado no mesmo método de "Verificação de Pe"
+							# e representa a coluna BT
 
 	def show(self):
 		print("Área: " + str(self.area) + "   cn: " + str(self.cn) + "   k: " + str(self.k) + "   n: " + str(self.n) + "   Ia: " + str(self.ia))
@@ -52,14 +63,13 @@ class SubBacia:
 
 	def calculaSmm(self):
 		self.s_mm = (float(25400)/self.cn) - 254
-		#print("s_mm: " + str(self.s_mm) + " cn: " + str(self.cn) + " div: " + str(float(25400)/self.cn))
 
 	def calculaHUI(self):
 		i = 0
 		tempHui = list(dt)
 		for value in self.hui:
 			#Coluna E
-			self.hui[i] = (1/(self.k*math.exp(math.lgamma(self.n))))*math.exp(-dt[i]/self.k)*math.pow((dt[i]/self.k),(self.n-1))
+			self.hui[i] = (1/(self.k*math.gamma(self.n)))*math.exp(-dt[i]/self.k)*math.pow((dt[i]/self.k),(self.n-1))
 
 			#Coluna F
 			# 10000000/60000 = 1000/6
@@ -81,38 +91,55 @@ class SubBacia:
 	def calculaVerificacaoPu(self):
 		#Verificação de Pu = 1mm   ↑
 		#=(SOMA(H3:H132)*30*60)/(D3*1000)
-		#				 180
+		#				 1800
 		soma = 0.0
 		for value in self.hui:
 			soma += value
-		self.verificacaoPu = (soma * 180) / (self.area * 1000)
+		self.verificacaoPu = float(soma * 1800) / float(self.area * 1000)
 
 	def calculaPAcum(self):
 		self.pacum.append(self.ia)
-
-		""" está certo este cálculo??"""
 		l = len(self.leituras)
 		i = 1
-		#for value in range(l):
 		while i < l:
-			#print("temp: " + str(temp) + " lastPacum: " + str(self.pacum[-1]) + " leituras[i]: " + str(self.leituras[i]))
-			self.pacum.append(self.pacum[-1] + self.leituras[i])	#lista[-1] retorna ultimo elemento da lista
+			#lista[-1] retorna ultimo elemento da lista
+			self.pacum.append(self.pacum[-1] + self.leituras[i])	
 			i+=1
 
 	def calculaPefacum(self):
 		for value in self.pacum:
-			#print("value: " + str(value) + " s_mm: " + str(self.s_mm) + " ia: " + str(self.ia))
 			self.pefacum.append( math.pow((value - self.ia), 2) / (value + self.s_mm - self.ia) )
 
 	def calculaPefIntervalo(self):
 		self.pefIntervalo.append(0.0)
 		l = len(self.pefacum)
-		#for i in range(l):
 		i = 1
 		while i < l: 
 			self.pefIntervalo.append(self.pefacum[i] - self.pefacum[i-1])
 			i+=1
 
+	def calculaQSimulado(self):
+		pefIntervaloLenght = len(self.pefIntervalo)
+		huiLenght = len(self.hui)
+		self.qSimulado = [0.0 for x in range(huiLenght)]
+		
+		i = 0
+		for pefIntervalo in self.pefIntervalo:
+			k = i
+			for hui in self.hui:
+				self.qSimulado[k] += (pefIntervalo / self.verificacaoPu) * hui 
+				k+=1
+			i+=1
+			self.qSimulado.append(0.0)
+		
+	def calculaVerificacaoPe(self):
+		soma = 0.0
+		self.qp = -1
+		for value in self.qSimulado:
+			soma += value
+			if value > self.qp:
+				self.qp = value
+		self.verificaçãoPe = (soma*1800) / (self.area*1000)
 
 	def calcula(self):
 		self.calculaHUI()
@@ -121,13 +148,10 @@ class SubBacia:
 		self.calculaSmm()
 		self.calculaPefacum()
 		self.calculaPefIntervalo()
+		self.calculaQSimulado()
+		self.calculaVerificacaoPe()
 
 
-
-
-
-	
-# Mudar tamanho das listas de hui para o tamanho da entrada lida para cada bacia e tal
 
 subBacias = initialize()
 
@@ -138,9 +162,11 @@ for sb in subBacias:
 	sb.calcula()
 	#print(sb.hui)
 	#print("sb.verificacaoPu: " + str(sb.verificacaoPu))
-	print(sb.pacum)
+	#print(sb.pacum)
 	#print(sb.pefacum)
-	print(sb.pefIntervalo)
+	#print(sb.pefIntervalo)
+	#print(sb.bq)
+	print(sb.verificaçãoPe)
 	i+=1
 
 
