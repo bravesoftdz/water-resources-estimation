f471a5dc979cd43b944d3321abf58cb33f0fc51a
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-12-03 21:04:39 -0200

    Realizado o somatório de todos valores de Q Simulado de todas as sub-bacias.

--- main.py
+++ main.py
@@ -17,9 +17,10 @@ for sb in subBacias:
 	i+=1
 
 soma = [0.0 for x in range(len(subBacias[0].qSimulado))]
-i = 0
 for sb in subBacias:
-	soma[i] += sb.qSimulado[i]
-	i+=1
+	i = 0
+	for value in sb.qSimulado:
+		soma[i] += value
+		i+=1
 
 print(soma)
