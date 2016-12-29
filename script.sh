32012dbc00de36b4153ff7a99d24764095219a40
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2016-12-29 18:02:57 -0200

    Script (podre) pra rodar 10 vezes o algoritmo automaticamente.

---
+++ script.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+echo "Execução 1"
+python3 main.py 
+echo "Execução 2"
+python3 main.py 
+echo "Execução 3"
+python3 main.py 
+echo "Execução 4"
+python3 main.py 
+echo "Execução 5"
+python3 main.py 
+echo "Execução 6"
+python3 main.py 
+echo "Execução 7"
+python3 main.py 
+echo "Execução 8"
+python3 main.py 
+echo "Execução 9"
+python3 main.py 
+echo "Execução 10"
+python3 main.py 
+
+
