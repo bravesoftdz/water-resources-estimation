9f05cd77e6926b6a00d5351f5647c2c66edc4287
Erick Moreira <efmoreira@inf.ufpel.edu.br>
2017-01-02 16:51:43 -0200

    Parser melhorado.

--- parser.py
+++ parser.py
@@ -6,22 +6,36 @@ def initialize(datafile='in.txt'):
 	""" Função para ler o arquivo de entrada, parseá-lo e retornar as n sub-bacias lidas dele. """
 	with open(datafile,'r') as f:
 		nBacias = f.readline()
+		nBacias = int(nBacias)
 		#print("nbacias: ",nBacias)
-		subBacias = [SubBacia() for i in range(int(nBacias))]
+		subBacias = [SubBacia() for i in range(nBacias)]
 		content = f.read().splitlines()
 
 	i = 0
-	for line in content:
+	for sb in subBacias:
+		#print(content[i])
+		line = content[i]
 		valores = line.split()
-		if(len(valores) >= 2):
-			subBacias[i].area = float(valores[0])
-			subBacias[i].cn = float(valores[1])
-			subBacias[i].k = float(valores[2])
-			subBacias[i].n = float(valores[3])
-			subBacias[i].ia = float(valores[4])
+		
+		sb.area = float(valores[0])
+		sb.cn = float(valores[1])
+		sb.k = float(valores[2])
+		sb.n = float(valores[3])
+		sb.ia = float(valores[4])
+		
+		i+=1
+		line = content[i]
+		valores = line.split()
+
+		while(len(valores) < 2):
+			sb.leituras.append(float(line))
 			i+=1
-		else:
-			subBacias[i-1].leituras.append(float(line))
+			if i < len(content):
+				line = content[i]
+				valores = line.split()
+			else:
+				break
+
 	return subBacias
 
 def readObserved(datafile='Q_ESD_Observada.txt'):
